84f7f9c8d8e0b1554103a8089173a15b2882c0af
김성은 <kse@gimseong-eun-ui-MacBookPro.local>
2024-05-15 22:07:39 +0900

    pls

--- README.md
+++
@@ -1,5 +0,0 @@
-# socialLoginProject
-
-에오스 과제
-
-ip : 16-171-116-34
--- config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    'COMPANY' : 'EOS',
-    'SERVER_PORT' : 3000,
-    'DB' : {
-        'DATABASE' : 'socialLoginProject',
-        'PORT' : 3306,
-        'DB_CONNECTION_LIMIT': 10
-    }
-}
